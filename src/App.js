4f80c404145ce523d97d293b472fedbb938f07ee
Preethi- Puttambaku <preethiputtambaku21@gmail.com>
2023-10-14 22:22:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleButtonClick = (value) => {
+    setInputValue(inputValue + value);
+  };
+
+  const handleClear = () => {
+    setInputValue('');
+  };
+
+  const handleCalculate = () => {
+    try {
+      setInputValue(eval(inputValue).toString());
+    } catch (error) {
+      setInputValue('Error');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='container'>
+        <div className='ipop'>
+          <input className='input' type='text' value={inputValue} readOnly></input>
+        </div>
+        <div className='digits'>
+          <table>
+            <tbody>
+              <tr>
+                <td><button onClick={() => handleButtonClick('7')}>7</button></td>
+                <td><button onClick={() => handleButtonClick('8')}>8</button></td>
+                <td><button onClick={() => handleButtonClick('9')}>9</button></td>
+                <td><button onClick={handleClear}>clr</button></td>
+              </tr>
+              <tr>
+                <td><button onClick={() => handleButtonClick('4')}>4</button></td>
+                <td><button onClick={() => handleButtonClick('5')}>5</button></td>
+                <td><button onClick={() => handleButtonClick('6')}>6</button></td>
+                <td><button onClick={() => handleButtonClick('/')}>/</button></td>
+              </tr>
+              <tr>
+                <td><button onClick={() => handleButtonClick('1')}>1</button></td>
+                <td><button onClick={() => handleButtonClick('2')}>2</button></td>
+                <td><button onClick={() => handleButtonClick('3')}>3</button></td>
+                <td><button onClick={() => handleButtonClick('-')}>-</button></td>
+              </tr>
+              <tr>
+                <td><button onClick={() => handleButtonClick('0')}>0</button></td>
+                <td><button onClick={() => handleButtonClick('.')}>.</button></td>
+                <td><button onClick={handleCalculate}>=</button></td>
+                <td><button onClick={() => handleButtonClick('+')}>+</button></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
